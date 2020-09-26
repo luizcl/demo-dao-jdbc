95e50c3444de6999989f730bca286f21b07e77b3
Luiz <luiz.cl@live.com>
2020-09-26 18:01:43 -0300

    findById implementation

--- src/model/impl/SellerDaoJDBC.java
+++ src/model/impl/SellerDaoJDBC.java
@@ -1,12 +1,24 @@
 package model.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
-
+	
+	private Connection con;
+	
+	public SellerDaoJDBC(Connection con) {
+		this.con = con;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
@@ -27,7 +39,40 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement st = null;
+		ResultSet rs = null; 
+		
+		try {
+			st = con.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " +
+					"FROM seller INNER JOIN department " + 
+					"ON seller.DepartmentId = department.Id " + 
+					"WHERE seller.Id = ?");
+			
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			
+			if(rs.next()) {
+				Department dep = new Department(rs.getInt("DepartmentId"), rs.getString("DepName"));
+				Seller sel = new Seller(
+						rs.getInt("Id"),
+						rs.getString("Name"),
+						rs.getString("Email"),
+						rs.getDate("BirthDate"),
+						rs.getDouble("BaseSalary"),
+						dep);
+				
+				return sel;
+			}
+			return null;
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 		return null;
 	}
 
