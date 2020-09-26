9fdca69394bc04655059e966001d5ddc9f3a8f94
Luiz <luiz.cl@live.com>
2020-09-26 18:09:50 -0300

    Reusing instantiation

--- src/model/impl/SellerDaoJDBC.java
+++ src/model/impl/SellerDaoJDBC.java
@@ -55,14 +55,8 @@ public class SellerDaoJDBC implements SellerDao{
 			rs = st.executeQuery();
 			
 			if(rs.next()) {
-				Department dep = new Department(rs.getInt("DepartmentId"), rs.getString("DepName"));
-				Seller sel = new Seller(
-						rs.getInt("Id"),
-						rs.getString("Name"),
-						rs.getString("Email"),
-						rs.getDate("BirthDate"),
-						rs.getDouble("BaseSalary"),
-						dep);
+				Department dep = instantiateDepartment(rs);
+				Seller sel = instantiateSeller(rs, dep);
 				
 				return sel;
 			}
@@ -74,6 +68,20 @@ public class SellerDaoJDBC implements SellerDao{
 			DB.closeResultSet(rs);
 		}
 		return null;
+	}
+	
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		return new Seller(
+				rs.getInt("Id"),
+				rs.getString("Name"),
+				rs.getString("Email"),
+				rs.getDate("BirthDate"),
+				rs.getDouble("BaseSalary"),
+				dep);
+	}
+	
+	private Department instantiateDepartment(ResultSet rs) throws SQLException{
+		return new Department(rs.getInt("DepartmentId"), rs.getString("DepName"));
 	}
 
 	@Override
